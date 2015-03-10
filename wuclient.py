fdf0ce0a8cf8582e88e21a9984b692970ea63785
Philip <pipcet@gmail.com>
2015-03-10 12:26:09 +0000

    argh. fix whitespace in python examples

--- wuclient.py
+++ wuclient.py
@@ -1,17 +1,18 @@
 #
-# Weather update client
-# Connects SUB socket to tcp://localhost:5556
-# Collects weather updates and finds avg temp in zipcode
+#   Weather update client
+#   Connects SUB socket to tcp://localhost:5556
+#   Collects weather updates and finds avg temp in zipcode
 #
 
 import sys
 import zmq
 
-# Socket to talk to server
+
+#  Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
-print("Collecting updates from weather server\u2026")
+print("Collecting updates from weather server...")
 socket.connect("tcp://localhost:5556")
 
 # Subscribe to zipcode, default is NYC, 10001
@@ -19,16 +20,16 @@ zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
 
 # Python 2 - ascii bytes to unicode str
 if isinstance(zip_filter, bytes):
-zip_filter = zip_filter.decode('ascii')
+    zip_filter = zip_filter.decode('ascii')
 socket.setsockopt_string(zmq.SUBSCRIBE, zip_filter)
 
 # Process 5 updates
 total_temp = 0
 for update_nbr in range(5):
-string = socket.recv_string()
-zipcode, temperature, relhumidity = string.split()
-total_temp += int(temperature)
+    string = socket.recv_string()
+    zipcode, temperature, relhumidity = string.split()
+    total_temp += int(temperature)
 
 print("Average temperature for zipcode '%s' was %dF" % (
-zip_filter, total_temp / update_nbr)
+      zip_filter, total_temp / update_nbr)
 )
