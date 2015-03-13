4bea7cbcdff101fa42c0bd431167e11a2cfc2747
Philip <pipcet@gmail.com>
2015-03-13 02:37:36 +0000

    self-contained (i.e. using a Perl loop rather than Benchmark) testing program for FFI.
    With the very hackish JIT code I just pushed to github:
    
    time perl ./zmq-bench-xsexec.pl; time perl ./zmq-bench-ffiexec.pl
    perl ./zmq-bench-xsexec.pl  48.49s user 0.38s system 99% cpu 48.912 total
    FFI ZMQ Version: 4.0.5
    perl ./zmq-bench-ffiexec.pl  40.36s user 0.05s system 99% cpu 40.436 total

---
+++ zmq-bench-ffiexec.pl
@@ -0,0 +1,60 @@
+#
+# should run for about 50 seconds.
+#
+
+use strict;
+use warnings;
+use v5.10;
+use ZMQ::FFI::Constants qw(:all);
+use FFI::TinyCC;
+use FFI::Platypus::Declare;
+
+lib 'libzmq.so';
+
+attach(
+    ['zmq_ctx_new' => 'zmqffi_ctx_new']
+	=> [] => 'pointer'
+);
+
+attach(
+    ['zmq_socket' => 'zmqffi_socket']
+	=> ['pointer', 'int'] => 'pointer'
+);
+
+attach(
+    ['zmq_bind' => 'zmqffi_bind']
+	=> ['pointer', 'string'] => 'int'
+);
+
+attach(
+  ['zmq_send' => 'zmqffi_send']
+  => ['pointer', 'string', 'size_t', 'int'] => 'int',
+    );
+
+attach(
+    ['zmq_version' => 'zmqffi_version']
+	=> ['int*', 'int*', 'int*'] => 'void'
+);
+
+our $ffi_ctx = main::zmqffi_ctx_new();
+die 'ffi ctx error' unless $ffi_ctx;
+
+our $ffi_socket = main::zmqffi_socket($ffi_ctx, ZMQ_PUB);
+die 'ffi socket error' unless $ffi_socket;
+
+my $rv;
+
+$rv = zmqffi_bind($ffi_socket, "ipc:///tmp/zmq-ffi-bench-$$");
+die 'ffi bind error' if $rv == -1;
+
+my ($major, $minor, $patch);
+zmqffi_version(\$major, \$minor, \$patch);
+
+say "FFI ZMQ Version: " . join(".", $major, $minor, $patch);
+
+my $i;
+while(1) {
+  $i++;
+  zmqffi_send($ffi_socket, 'ohhai', 5, 0);
+  exit if $i == 100_000_000;
+}
